8f0cc73231187ec9d3a9f318043ce0f52116e791
jy <jy@jys-MacBook-Pro.local>
2026-05-14 14:42:59 +0800

    add: event bus

---
+++ custom_events/lifecycle_js_vue/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="hello">Hello</div>
+    <h1>{{ count }}</h1>
+    <button @click="count++" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      information: "info",
+    };
+  },
+
+  created() {
+    console.log("created", this.information);
+    const doms = document.querySelector("hello");
+    console.log("doms", doms);
+  },
+  mounted() {
+    this.loadData();
+  },
+  updated() {
+    console.log("updated");
+  },
+};
+</script>
