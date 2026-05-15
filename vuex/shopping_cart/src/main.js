340e5b1c52a3e03ca1d991874719d762b9476202
yan zhou <15930598930@163.com>
2026-05-15 17:42:17 +0800

    add:serve

--- vuex/shopping_cart/src/main.js
+++ vuex/shopping_cart/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
-import App from './app-complete/App.vue';
-import store from './app-complete/store';
+// import App from './app-complete/App.vue';
+// import store from './app-complete/store';
+import App from './app/App.vue'
 
-createApp(App).use(store).mount('#app');
+// createApp(App).use(store).mount('#app');
+createApp(App).mount('#app');
+
