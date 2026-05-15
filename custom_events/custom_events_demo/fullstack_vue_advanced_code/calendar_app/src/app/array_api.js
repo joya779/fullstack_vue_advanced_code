5e6b168bc4c20f743f9ba5817b3db1583b0fe24e
yan zhou <15930598930@163.com>
2026-05-15 15:15:51 +0800

    del

--- custom_events/custom_events_demo/fullstack_vue_advanced_code/calendar_app/src/app/array_api.js
+++
@@ -1,7 +0,0 @@
-const array = [1,2,3,4]
-// console.log(array.fill(5, 2))
-const res = array.filter((a) => a>2)
-// console.log(res);
-const find_res = array.find(a => a === '2')
-console.log(find_res);
-
--- custom_events/custom_events_demo/fullstack_vue_advanced_code/calendar_app/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import App from './app-complete/App.vue';
-// import App from "./app/App.vue";
-
-createApp(App).mount("#app");
